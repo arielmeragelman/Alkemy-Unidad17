eb0943d1cafebe253f740d461532527ce2c1b9bd
Sebastian Meragelman <arielmeragelman@gmail.com>
2022-11-11 15:47:41 -0300

    se agrega metodo para lectura de logs

--- app.py
+++ app.py
@@ -1,5 +1,23 @@
 from flask import Flask
+
+
 app = Flask(__name__)
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def hello_world() -> str:
+    """hello_world Saludo de inicio
+
+    :return: Devuelve el saludo de inicio de la api
+    :rtype: str
+    """
+    return 'Pagina Inicio!'
+
+@app.route('/logs')
+def mostrar_logs() -> list:
+    """mostrar_logs Devuelve los registros del archivo error_logging.log
+
+    :return: Lista de errores registrados
+    :rtype: list
+    """
+
+    with open(r"logs/error_loggin.log", "r") as f:
+        return f.read().split("\n")
